752b642a699b437c2ab85815d78310eaf705012c
Ryan Yep <ryan.yep@gmail.com>
2015-09-12 19:09:48 -0500

    Displays previously inputted todos at beginning of prompt

--- bin/todo.rb
+++ bin/todo.rb
@@ -4,6 +4,9 @@ require_relative '../lib/all'
 class Todo
 
   def run_todo
+    puts "Current Todo list:"
+    display_todos
+    puts "-" * 20
     puts "What task would you like to add to your Todo app?"
     tasks = gets.chomp
     add_todo(tasks)
@@ -13,6 +16,21 @@ class Todo
   def add_todo(tasks)
     Todo.new(task: tasks).save
   end
+
+  def display_todos
+    Todo.all.each do |tasks|
+      puts tasks.task
+    end
+  end
 end
 
 Todo.new.run_todo
+
+# ~> Errno::ENOENT
+# ~> No such file or directory @ rb_sysopen - config/database.yml
+# ~>
+# ~> /Users/ryep/theironyard/week3/day4/Todo/db/setup.rb:4:in `initialize'
+# ~> /Users/ryep/theironyard/week3/day4/Todo/db/setup.rb:4:in `open'
+# ~> /Users/ryep/theironyard/week3/day4/Todo/db/setup.rb:4:in `<top (required)>'
+# ~> /Users/ryep/theironyard/week3/day4/Todo/bin/todo.rb:1:in `require_relative'
+# ~> /Users/ryep/theironyard/week3/day4/Todo/bin/todo.rb:1:in `<main>'

0e498136c7a14427f5cdc94337c2f4f64297b8d4
Ryan Yep <ryan.yep@gmail.com>
2015-09-14 09:19:41 -0500

    Working prompt to ask if you want to add a todo or display your inputted list of todos

--- bin/todo.rb
+++ bin/todo.rb
@@ -4,20 +4,30 @@ require_relative '../lib/all'
 class Todo
 
   def run_todo
-    puts "Current Todo list:"
-    display_todos
     puts "-" * 20
-    puts "What task would you like to add to your Todo app?"
-    tasks = gets.chomp
-    add_todo(tasks)
-    puts "You just added as a task: #{tasks}"
+    puts "What task would you like to do?"
+    puts "1.) Exit 2.) Add Todo 3.) See list of Todo's"
+    print " > "
+    response = gets.chomp.to_i
+    case response
+    when 1 then exit
+    when 2 then add_todo
+    when 3 then display_todos
+    else
+      puts "\a"
+      puts "Not a valid choice"
+    end
   end
 
-  def add_todo(tasks)
+  def add_todo
+    puts "Please add a todo > "
+    tasks = gets.chomp
     Todo.new(task: tasks).save
   end
 
   def display_todos
+    puts "List of Todo's:"
+    puts "-" * 20
     Todo.all.each do |tasks|
       puts tasks.task
     end
@@ -25,12 +35,3 @@ class Todo
 end
 
 Todo.new.run_todo
-
-# ~> Errno::ENOENT
-# ~> No such file or directory @ rb_sysopen - config/database.yml
-# ~>
-# ~> /Users/ryep/theironyard/week3/day4/Todo/db/setup.rb:4:in `initialize'
-# ~> /Users/ryep/theironyard/week3/day4/Todo/db/setup.rb:4:in `open'
-# ~> /Users/ryep/theironyard/week3/day4/Todo/db/setup.rb:4:in `<top (required)>'
-# ~> /Users/ryep/theironyard/week3/day4/Todo/bin/todo.rb:1:in `require_relative'
-# ~> /Users/ryep/theironyard/week3/day4/Todo/bin/todo.rb:1:in `<main>'
